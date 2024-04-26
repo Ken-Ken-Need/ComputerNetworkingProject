d74dd37b75173e4223bd70765b93fad9a4cb2bec
Ken <kg3209@nyu.edu>
2024-04-26 13:04:47 +0800

    Flags half way thru

--- src/decoder/dns.py
+++ src/decoder/dns.py
@@ -68,21 +68,91 @@ decodedDNSData = TypedDict(
 )
 
 
-def bytearray_to_binary_string(byte_array):
-    return (
-        int.from_bytes(byte_array, byteorder="big")
-        .to_bytes((len(byte_array) + 7) // 8, byteorder="big")
-        .decode("utf-8")
-    )
+def bytearray_to_string(byte_array: bytearray):
+    return byte_array.decode("utf-8")
+
+
+def hex_to_binary(hex_string):
+    binary_string = ""
+    hex_to_bin_mapping = {
+        "0": "0000",
+        "1": "0001",
+        "2": "0010",
+        "3": "0011",
+        "4": "0100",
+        "5": "0101",
+        "6": "0110",
+        "7": "0111",
+        "8": "1000",
+        "9": "1001",
+        "A": "1010",
+        "B": "1011",
+        "C": "1100",
+        "D": "1101",
+        "E": "1110",
+        "F": "1111",
+    }
+    hex_string = hex_string.lstrip("0x")
+    for char in hex_string:
+        char_upper = char.upper()
+        binary_string += hex_to_bin_mapping.get(char_upper, "")
+    return binary_string
+
+
+def decodeFlag(binary_representation: str):
+    Flags: str = ""
+
+    Flags += "Response: "
+    if binary_representation[0] == 0:
+        Flags += "Query\n"
+    else:
+        Flags += "Response"
+
+    opcode = binary_representation[1:5]
+    Flags += "Opcode: "
+    if opcode == "0000":
+        Flags += "Standard query\n"
+    elif opcode == "0001":
+        Flags += "Inverse query\n"
+    elif opcode == "0010":
+        Flags += "Server status request\n"
+    elif opcode == "0011":
+        Flags += "Status reserved and therefore not used\n"
+    else:
+        Flags += opcode
+
+    Flags += "Authoritive: "
+    if opcode[5] == "0":
+        Flags += "The server is not an authority for the domain\n"
+    else:
+        Flags += "The server is an authority for the domain\n"
+
+    if binary_representation[6] == "0":
+        Flags += "Truncated: The message is not truncated\n"
+    else:
+        Flags += "Truncated: The message is truncated\n"
+
+    if binary_representation[7] == "0":
+        Flags += "Recursion desired: Do not query recursively\n"
+    else:
+        Flags += "Recursion desired: Do query recursively\n"
+
+    if binary_representation[8] == "0":
+        Flags += "Recursion available: Server can do recursive queries\n"
+    else:
+        Flags += "Recursion available: Server can not do recursive queries\n"
+
+    return Flags
 
 
 def decodeDNSData(data: segmentedDNSData) -> decodedDNSData:
+    """The part to process Transaction ID"""
     TransactionID: str = "0x" + data["Transaction ID"]
 
+    # -------Flags------------#
     original_hex: str = data["Flags"]
-    binary_representation: str = bytearray_to_binary_string(original_hex)
-    print(binary_representation)
-
+    binary_representation: str = hex_to_binary(original_hex)
+    Flags: str = decodeFlag(binary_representation)
     # return {
     #     "Transaction ID": data["Transaction ID"],
     #     "Flags": data["Flags"],
